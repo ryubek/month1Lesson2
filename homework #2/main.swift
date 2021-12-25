a42d570ef67f7c02cdb4d3901ec9ecad419b77b4
Beksultan <96584261+ryubek@users.noreply.github.com>
2021-12-25 09:30:20 +0600

    Update main.swift
    Выдает ошибку при pull. Показывает дефолтное "Hello world". Решение: скопировал из файла и вставил в гит репозиторий вручную.

--- homework #2/main.swift
+++ homework #2/main.swift
@@ -7,5 +7,19 @@
 
 import Foundation
 
-print("Hello, World!")
+var bankAccount = 22000
+    print("Ваш баланс составляет: 22000 $")
+
+print("Введите сумму снятия с баланса")
+var operation = Int(readLine()!)
+
+let balance = Int(bankAccount) - Int(operation ?? 0)
+
+if ( operation ?? 0 == 22000){
+    print("Остаток составляет 0 $")
+} else if ( operation ?? 0 > 22000){
+    print("У вас недостаточно средств")
+} else if ( operation ?? 0 < 22000){
+    print("Вы сняли:", operation!, "$", ", ваш остаток составляет:", balance, "$" )
+}
 
